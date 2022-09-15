f6ef0d755713d73fd301d87feba563970d5e7173
Aitor Estarrona <aes3297@gmail.com>
2022-09-15 13:01:30 +0200

    The page footer now becomes a right off-canvas on small screens.

--- include/footer.php
+++ include/footer.php
@@ -1,38 +1,43 @@
-<footer class="mt-5 py-5 bg-ffmidnight text-white text-center">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <p><?php echo $lang["lang.footer.organized-by"]; ?></p>
-        <h4><a class="text-reset text-decoration-none" href="organizadores.php"><?php echo $lang["lang.footer.valinor"]; ?></a></h4>
-        <p class="my-4"><?php echo $lang["lang.footer.contact"]; ?></p>
-        <ul class="list-inline mb-0">
-          <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="https://www.facebook.com/asociacionvalinor"><i class="bi bi-facebook"></i></a></li>
-          <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="https://www.instagram.com/asovalinor"><i class="bi bi-instagram"></i></a></li>
-          <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="mailto:asovalinor@gmail.com"><i class="bi bi-envelope-fill"></i></a></li>
-          <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="https://twitter.com/AsoValinor"><i class="bi bi-twitter"></i></a></li>
-        </ul>
-      </div>
-    </div>
-
-    <hr class="my-5">
-
-    <div class="row row-cols-2 row-cols-lg-4 gy-4">
-      <div class="col d-flex justify-content-center align-items-center">
-        <img class="logo-sponsor" src="/images/logos/Logo_Gauekoak-min.png" alt="" width="auto" height="40">
+<footer class="offcanvas-lg offcanvas-end bg-ffmidnight text-white text-center mt-lg-5 py-4 py-lg-5" tabindex="-1" id="footerOffcanvas">
+  <div class="offcanvas-header text-light px-4">
+    <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" data-bs-target="#footerOffcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body px-4 px-lg-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <p><?php echo $lang["lang.footer.organized-by"]; ?></p>
+          <h4><a class="text-reset text-decoration-none" href="organizadores.php"><?php echo $lang["lang.footer.valinor"]; ?></a></h4>
+          <p class="my-4"><?php echo $lang["lang.footer.contact"]; ?></p>
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="https://www.facebook.com/asociacionvalinor"><i class="bi bi-facebook"></i></a></li>
+            <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="https://www.instagram.com/asovalinor"><i class="bi bi-instagram"></i></a></li>
+            <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="mailto:asovalinor@gmail.com"><i class="bi bi-envelope-fill"></i></a></li>
+            <li class="list-inline-item"><a class="btn btn-lg btn-outline-light btn-outline-b" href="https://twitter.com/AsoValinor"><i class="bi bi-twitter"></i></a></li>
+          </ul>
+        </div>
       </div>
 
-      <div class="col d-flex justify-content-center align-items-center">
-        <img class="logo-sponsor rounded-3" src="/images/logos/marcaVG!greencapital-BicolorNegativo-min.jpg" alt="" width="auto" height="40">
-      </div>
+      <hr class="my-5">
 
-      <div class="col d-flex justify-content-center align-items-center">
-        <img class="logo-sponsor rounded-3" src="/images/logos/MarcaAytoNegativo-H-min.jpg" alt="" width="auto" height="40">
-      </div>
+      <div class="row row-cols-2 row-cols-lg-4 gy-4">
+        <div class="col d-flex justify-content-center align-items-center">
+          <img class="logo-sponsor" src="/images/logos/Logo_Gauekoak-min.png" alt="" width="auto" height="40">
+        </div>
 
-      <div class="col d-flex justify-content-center align-items-center">
-        <a href="https://www.fundacionvital.eus/">
-          <img class="logo-sponsor bg-light rounded-3 p-2 p-lg-3" src="/images/logos/fundacion-vital-logo-pie.svg" alt="" width="auto" height="40">
-        </a>
+        <div class="col d-flex justify-content-center align-items-center">
+          <img class="logo-sponsor rounded-3" src="/images/logos/marcaVG!greencapital-BicolorNegativo-min.jpg" alt="" width="auto" height="40">
+        </div>
+
+        <div class="col d-flex justify-content-center align-items-center">
+          <img class="logo-sponsor rounded-3" src="/images/logos/MarcaAytoNegativo-H-min.jpg" alt="" width="auto" height="40">
+        </div>
+
+        <div class="col d-flex justify-content-center align-items-center">
+          <a href="https://www.fundacionvital.eus/">
+            <img class="logo-sponsor bg-light rounded-3 p-2 p-lg-3" src="/images/logos/fundacion-vital-logo-pie.svg" alt="" width="auto" height="40">
+          </a>
+        </div>
       </div>
     </div>
   </div>
--- include/header.php
+++ include/header.php
@@ -3,13 +3,13 @@
     <button id="nav-toggler" class="navbar-toggler p-2 border-0 shadow-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvas" aria-controls="navbarOffcanvas" aria-expanded="false" aria-label="Toggle navigation">
       <i class="bi bi-list" style="font-size: 2rem;"></i>
     </button>
-    <a id="nav-logo" class="navbar-brand mx-auto mx-lg-0 pe-5 pe-lg-0" href="index.php">
+    <a id="nav-logo" class="navbar-brand mx-auto mx-lg-0" href="index.php">
       <img src="/images/logo_freak_festival_22.svg" class="navbar-logo" alt="Freak Festival" width="102" height="40">
     </a>
     <div class="offcanvas offcanvas-start bg-ffmidnight d-lg-contents" tabindex="-1" id="navbarOffcanvas" aria-labelledby="offcanvasTitle">
       <div class="offcanvas-header text-light px-4 pb-0">
         <img src="/images/logo_freak_festival_22.svg" id="offcanvasTitle" alt="Freak Festival" width="153" height="60">
-        <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" data-bs-target="#navbarOffcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body d-lg-contents p-4 pt-0 p-lg-0">
         <hr class="d-lg-none text-white-50">
@@ -17,7 +17,9 @@
           <ul class="navbar-nav justify-content-lg-evenly align-items-lg-center w-100">
             <li class="nav-item">
               <a class="nav-link f-frutiger-b text-uppercase text-lg-center
-              <?php if ($curPageName == "index.php") echo "active\" aria-current=\"page"; ?>" href="index.php"><?php echo $lang["lang.header.home"]; ?></a>
+              <?php if ($curPageName == "index.php") echo "active\" aria-current=\"page"; ?>" href="index.php">
+                <?php echo $lang["lang.header.home"]; ?>
+              </a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link f-frutiger-b text-uppercase text-lg-center dropdown-toggle
@@ -32,7 +34,9 @@
             </li>
             <li class="nav-item">
               <a class="nav-link f-frutiger-b text-uppercase text-lg-center
-              <?php if ($curPageName == "invitados.php") echo "active\" aria-current=\"page"; ?>" href="invitados.php"><?php echo $lang["lang.header.guests"]; ?></a>
+              <?php if ($curPageName == "invitados.php") echo "active\" aria-current=\"page"; ?>" href="invitados.php">
+                <?php echo $lang["lang.header.guests"]; ?>
+              </a>
             </li>
           </ul>
         </div>
@@ -63,7 +67,7 @@
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link f-frutiger-b text-lowercase nav-link-lang dropdown-toggle" href="#" id="navbarDropdown-lang" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-label="Idioma seleccionado: español">
-              <?php echo $lang["lang.header.lang." . $lang["lang"]]; ?>
+                <?php echo $lang["lang.header.lang." . $lang["lang"]]; ?>
               </a>
               <ul class="dropdown-menu dropdown-menu-lg-end rounded-4 p-1" aria-labelledby="navbarDropdown-lang">
                 <li class="p-1"><a class="dropdown-item rounded-3 py-2 px-3" href="/<?php echo $curPageName; ?>"><?php echo $lang["lang.header.lang.es"]; ?></a></li>
@@ -74,8 +78,8 @@
         </div>
       </div>
     </div>
-    <!-- <div id="footer-toggler" class="d-lg-none invisible px-2 p-lg-0">
-      <i class="bi bi-three-dots-vertical" style="font-size: 2rem;"></i>
-    </div> -->
+    <button id="footer-toggler" class="navbar-toggler d-lg-none p-2 border-0 shadow-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#footerOffcanvas" aria-controls="footerOffcanvas" aria-expanded="false" aria-label="Toggle footer">
+      <i class="bi bi-three-dots-vertical" style="font-size: 1.5rem;"></i>
+    </button>
   </nav>
 </header>
