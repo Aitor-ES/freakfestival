9ebeadd3ebb1fc734de745914f640c1ff96d9f65
Aitor Estarrona <aes3297@gmail.com>
2023-04-17 19:08:51 +0200

    Decreased size of registration and candidacy buttons.

--- include/inscripciones-candidaturas.php
+++ include/inscripciones-candidaturas.php
@@ -10,10 +10,10 @@
     <p><?php echo $lang["lang.registrations-applications.description.1"]; ?></p>
     <p><?php echo $lang["lang.registrations-applications.description.2"]; ?></p>
 
-    <div class="mb-3"><a class="btn btn-lg btn-outline-dark btn-outline-b" href="https://forms.gle/tebtLyCWiHL13an18"><?php echo $lang["lang.registrations-applications.associations"]; ?></a></div>
-    <div class="mb-3"><a class="btn btn-lg btn-outline-dark btn-outline-b" href="https://forms.gle/bXm26z85xWmguWGX6"><?php echo $lang["lang.registrations-applications.activities"]; ?></a></div>
-    <div class="mb-3"><a class="btn btn-lg btn-outline-dark btn-outline-b" href="https://forms.gle/3fM1n3kTpkoU3Jeb8"><?php echo $lang["lang.registrations-applications.artists"]; ?></a></div>
-    <div class="mb-3"><a class="btn btn-lg btn-outline-dark btn-outline-b" href="https://forms.gle/oiEwtGQ9jBdEbJes6"><?php echo $lang["lang.registrations-applications.shops"]; ?></a></div>
+    <div class="mb-3"><a class="btn btn-outline-dark btn-outline-b" href="https://forms.gle/tebtLyCWiHL13an18"><?php echo $lang["lang.registrations-applications.associations"]; ?></a></div>
+    <div class="mb-3"><a class="btn btn-outline-dark btn-outline-b" href="https://forms.gle/bXm26z85xWmguWGX6"><?php echo $lang["lang.registrations-applications.activities"]; ?></a></div>
+    <div class="mb-3"><a class="btn btn-outline-dark btn-outline-b" href="https://forms.gle/3fM1n3kTpkoU3Jeb8"><?php echo $lang["lang.registrations-applications.artists"]; ?></a></div>
+    <div class="mb-3"><a class="btn btn-outline-dark btn-outline-b" href="https://forms.gle/oiEwtGQ9jBdEbJes6"><?php echo $lang["lang.registrations-applications.shops"]; ?></a></div>
   </div>
   <!-- Main End -->
 
