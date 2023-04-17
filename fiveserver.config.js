e9b976a857304f8e33536d01a2c9a303ffff3665
Aitor Estarrona <aes3297@gmail.com>
2023-04-17 19:17:43 +0200

    Fixed deployment.

--- fiveserver.config.js
+++
@@ -1,8 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-    php: "C:\\php-8.1.5-nts-Win32-vs16-x64\\php.exe",   // Windows
-    root: '',
-    open: 'index.php',
-    // highlight: true, // enable highlight feature
-    injectBody: true // enable instant update
-};
