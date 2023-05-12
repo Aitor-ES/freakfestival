d9467dbb4794b26e6d906d83c2f8ee0208b8f0ae
Aitor Estarrona <aes3297@gmail.com>
2023-05-12 17:43:15 +0200

    Added fourth 2023 guest.

--- include/invitados-2023.php
+++ include/invitados-2023.php
@@ -7,8 +7,8 @@
   <div class="container-md ff-main-gutter my-5">
     <h1 class="f-uncial-r mb-5"><?php echo $lang["lang.guests.title.2023"]; ?></h1>
 
-    <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-4 justify-content-evenly">
-      <div class="col">
+    <div class="row gy-5 justify-content-evenly">
+      <div class="col-12 col-lg-4">
         <div class="card h-100 bg-ffmidnight-gradient-top text-white border-0 rounded-4">
           <img src="/images/guests/2023_qrolanp-min.jpg" class="card-img-top" alt="<?php echo $lang["lang.guests.qrolanp.title"]; ?>">
 
@@ -28,7 +28,7 @@
         </div>
       </div>
 
-      <div class="col">
+      <div class="col-12 col-lg-4">
         <div class="card h-100 bg-ffmidnight-gradient-top text-white border-0 rounded-4">
           <img src="/images/guests/2023_destrona2plus-min.jpg" class="card-img-top" alt="<?php echo $lang["lang.guests.destrona2plus.title"]; ?>">
 
@@ -48,7 +48,10 @@
         </div>
       </div>
 
-      <div class="col">
+      <!-- Force next columns to break to new line -->
+      <div class="w-100 mt-0"></div>
+
+      <div class="col-12 col-lg-4">
         <div class="card h-100 bg-ffmidnight-gradient-top text-white border-0 rounded-4">
           <img src="/images/guests/2023_eneko_menica-min.jpg" class="card-img-top" alt="<?php echo $lang["lang.guests.eneko-menica.title"]; ?>">
 
@@ -70,6 +73,31 @@
 
               <div class="col-auto">
                 <a class="btn btn-lg btn-outline-ffsunlight btn-outline-0" href="https://www.youtube.com/enekomenica" role="button"><i class="bi bi-youtube"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-lg-4">
+        <div class="card h-100 bg-ffmidnight-gradient-top text-white border-0 rounded-4">
+          <img src="/images/guests/2023_usagitxo-min.jpg" class="card-img-top" alt="<?php echo $lang["lang.guests.usagitxo.title"]; ?>">
+
+          <div class="card-body">
+            <h5 class="card-title"><?php echo $lang["lang.guests.usagitxo.title"]; ?></h5>
+            <h6 class="card-subtitle mb-2"><?php echo $lang["lang.guests.usagitxo.dedication"]; ?></h6>
+            <p class="card-text"><?php echo $lang["lang.guests.usagitxo.description.1"]; ?></p>
+            <p class="card-text"><?php echo $lang["lang.guests.usagitxo.description.2"]; ?></p>
+          </div>
+
+          <div class="card-footer">
+            <div class="row g-2">
+              <div class="col-auto">
+                <a class="btn btn-lg btn-outline-ffsunlight btn-outline-0" href="https://www.instagram.com/usagitxocosplay/" role="button"><i class="bi bi-instagram"></i></a>
+              </div>
+
+              <div class="col-auto">
+                <a class="btn btn-lg btn-outline-ffsunlight btn-outline-0" href="https://www.facebook.com/Usagitxo/" role="button"><i class="bi bi-facebook"></i></a>
               </div>
             </div>
           </div>
