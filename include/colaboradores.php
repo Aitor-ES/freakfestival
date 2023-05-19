fa1fb8323675bf446740f536a51d5f9bea40204b
Aitor Estarrona <aes3297@gmail.com>
2023-05-19 16:21:59 +0200

    Revealed first 2023 contributors.

--- include/colaboradores.php
+++ include/colaboradores.php
@@ -7,111 +7,95 @@
   <div class="container-md ff-main-gutter my-5">
     <h1 class="f-uncial-r mb-5"><?php echo $lang["lang.partners.title"]; ?></h1>
 
-    <div class="text-center py-5">
-      <h2 class="my-5"><?php echo $lang["lang.common.soon"]; ?></h2>
+    <div class="text-center">
+      <h2 class="my-5"><?php echo $lang["lang.common.wip"]; ?></h2>
     </div>
 
-    <!-- <div class="row gy-4 justify-content-center">
-      <div class="col-auto">
+    <div class="row gy-4 justify-content-center">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/Abaco/LogoAbaco1.jpeg" alt="Abaco" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/Atlas/LogoATLAS1.jpeg" alt="Atlas" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/BattleBunker/Logo morado BBunker Completo.png" alt="BattleBunker" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/CSE/EscudoCSE.jpg" alt="CSE" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/Kuaizi/KUAIZI LOGO TRANS.png" alt="Kuaizi" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/Orden de Revan/logo_color-1.png" alt="Orden de Revan" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Asos/StrongTales/STRONGTALES-1.png" alt="StrongTales" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/AV escape room/IMG_20220608_130802.jpg" alt="AV escape room" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Cerbero Editorial/logo2.png" alt="Cerbero Editorial" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Corvus Belli/logoCorvus.png" alt="Corvus Belli" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Devir/logo-devir-azul-fondo-transparente.png" alt="Devir" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/El rastro de la piel/logo-fondo-transparente.png" alt="El rastro de la piel" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Entretelas/LOGO ENTRETELAS.png" alt="Entretelas" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Green Stuff World/GSW-Logo-Basic-Black.png" alt="Green Stuff World" width="auto" height="60px">
-      </div>
+      </div> -->
 
       <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Marcas/Hipsteria/LogoHipsteria1.jpeg" alt="Hipsteria" width="auto" height="60px">
+        <img class="logo-contributor" src="/images/logos/contributors/Marcas/Hipsteria/LogoHipsteria1-min.jpeg" alt="Hipsteria" width="auto" height="60px">
       </div>
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Magic All Goal/magicallgoal_logo.png" alt="Magic All Goal" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/Maldito Games/maldito-horizontal (1).png" alt="Maldito Games" width="auto" height="60px">
-      </div>
+      </div> -->
 
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Marcas/NoSoloRol/Copia de LogosB.png" alt="NoSoloRol" width="auto" height="60px">
+      </div> -->
+
+      <div class="col-auto">
+        <img class="logo-contributor" src="/images/logos/contributors/Marcas/Omega/OMEGALOGO-VITORIA(2)-min.jpg" alt="Omega" width="auto" height="60px">
       </div>
 
       <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Marcas/Omega/OMEGALOGO-VITORIA(2).jpg" alt="Omega" width="auto" height="60px">
+        <img class="logo-contributor" src="/images/logos/contributors/Marcas/Zuloa/Zuloa-min.png" alt="Zuloa" width="auto" height="60px">
       </div>
 
-      <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Marcas/Zuloa.png" alt="Zuloa" width="auto" height="60px">
-      </div>
-
-      <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Restaurantes/Basque.png" alt="Basque" width="auto" height="60px">
-      </div>
-
-      <div class="col-auto">
+      <!-- <div class="col-auto">
         <img class="logo-contributor" src="/images/logos/contributors/Restaurantes/CachitoMio.png" alt="CachitoMio" width="auto" height="60px">
-      </div>
-
-      <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Restaurantes/CBK logo efecto REGISTRADA.png" alt="CBK" width="auto" height="60px">
-      </div>
-
-      <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Restaurantes/Dorsia/IMG-20201021-WA0006.jpg" alt="Dorsia" width="auto" height="60px">
-      </div>
-
-      <div class="col-auto">
-        <img class="logo-contributor" src="/images/logos/contributors/Restaurantes/Helados Plaza.png" alt="Helados Plaza" width="auto" height="60px">
-      </div>
-    </div> -->
+      </div> -->
+    </div>
   </div>
   <!-- Main End -->
 
