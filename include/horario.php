3203ddffac8a1da953418609aaa9bc32dad5c216
Aitor Estarrona <aes3297@gmail.com>
2023-06-10 14:27:32 +0200

    Improved timetable image quality.

--- include/horario.php
+++ include/horario.php
@@ -8,13 +8,13 @@
     <h1 class="f-uncial-r mb-5"><?php echo $lang["lang.timetable.title"]; ?></h1>
 
     <!-- Horario -->
-    <div class="row gy-5">
-      <div class="col-12">
-        <img src="/images/timetables/Horario freak sabado-01.png" alt="" width="100%" height="auto">
+    <div class="row justify-content-center gy-5">
+      <div class="col-12 col-lg-8">
+        <img src="/images/timetables/Horario freak sabado-02.png" alt="" width="100%" height="auto">
       </div>
 
-      <div class="col-12">
-        <img src="/images/timetables/Horario freak domingo-01.png" alt="" width="100%" height="auto">
+      <div class="col-12 col-lg-8">
+        <img src="/images/timetables/Horario freak domingo-02.png" alt="" width="100%" height="auto">
       </div>
     </div>
   </div>
