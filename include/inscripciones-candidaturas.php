602caec2cf72297efa2f087104f87c8b0939d80c
Aitor Estarrona <aes3297@gmail.com>
2024-03-10 13:54:03 +0100

    Removed abandonded CSS class btn-block.

--- include/inscripciones-candidaturas.php
+++ include/inscripciones-candidaturas.php
@@ -11,10 +11,10 @@
     <p class="lead"><?php echo $lang["lang.registrations-applications.description.2"]; ?></p>
 
     <div class="d-flex flex-column align-items-lg-baseline gap-3">
-      <a class="btn btn-block btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/uBpSYT3TvpxTj9An8"><?php echo $lang["lang.registrations-applications.associations"]; ?></a>
-      <a class="btn btn-block btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/FvvvDUNfzQ8GZ5uS9"><?php echo $lang["lang.registrations-applications.activities"]; ?></a>
-      <a class="btn btn-block btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/Ebk5qwjpoPxk5yE67"><?php echo $lang["lang.registrations-applications.artists"]; ?></a>
-      <a class="btn btn-block btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/7KcUNHTS2eWFgPzG9"><?php echo $lang["lang.registrations-applications.shops"]; ?></a>
+      <a class="btn btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/uBpSYT3TvpxTj9An8"><?php echo $lang["lang.registrations-applications.associations"]; ?></a>
+      <a class="btn btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/FvvvDUNfzQ8GZ5uS9"><?php echo $lang["lang.registrations-applications.activities"]; ?></a>
+      <a class="btn btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/Ebk5qwjpoPxk5yE67"><?php echo $lang["lang.registrations-applications.artists"]; ?></a>
+      <a class="btn btn-outline-ffscarlet btn-outline-b" href="https://forms.gle/7KcUNHTS2eWFgPzG9"><?php echo $lang["lang.registrations-applications.shops"]; ?></a>
     </div>
   </div>
   <!-- Main End -->
