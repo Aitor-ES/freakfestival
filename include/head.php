25069b6d8dbb61d688297ea0f96f073c92da85a6
Aitor Estarrona <aes3297@gmail.com>
2025-03-01 12:20:22 +0100

    perf: merge all Google Fonts in one request

--- include/head.php
+++ include/head.php
@@ -36,15 +36,10 @@
 <link rel="preconnect" href="https://rsms.me/">
 <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-<!-- Google Fonts CSS -->
+<!-- Google Fonts CSS - Outfit, Uncial Antiqua -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-<!-- Outfit font CSS -->
-<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
-
-<!-- Uncial Antiqua font CSS -->
-<link href="https://fonts.googleapis.com/css2?family=Uncial+Antiqua&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Uncial+Antiqua&display=swap" rel="stylesheet">
 
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
